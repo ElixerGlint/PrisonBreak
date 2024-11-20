3c3ed8894facd0ecc0742b8f5196d2815bcebfc8
Elixer <everett.rumery@gmail.com>
2024-11-20 14:05:30 -0500

    asgdg

--- src/Driver.java
+++ src/Driver.java
@@ -1,5 +1,16 @@
 public class Driver {
     public static void main(String[] args) throws Exception {
-        
+        Prison abc = new Prison(100);
+        System.out.println(abc);
+
+
+
+
+
+
+
+
+
+
     }
 }
--- src/Prison.java
+++ src/Prison.java
@@ -1,23 +1,35 @@
+import java.util.Arrays;
+
 public class Prison {
-    int arr[];
+    private int[] boxes;
 
+    public Prison(int boxnum) {
+        boxes = new int[boxnum];
+        for (int i = 0; i < boxnum; i++) {
+            boxes[i] = (int) (Math.random() * boxnum);
+        }
+    }
 
+    @Override
+    public String toString() {
+        String output = "";
+        for(int i = 0; i < boxes.length; i++) {
 
+            if(i < 10) {
+                output+= "[0" + i + "]";
+            }
+            else{
+                output+= "[" + i + "]";
+            }
+            if(i%10 == 0 && i > 0) {
+                output+="\n";
+                for(int j = i-10; j < i; j++) {
+                    output+= "[" + boxes[j] + "]";
+                }
+                output += "\n";
+            }
+        }
+        return output;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
+    }
 }
