5e8daa982e6f4b4da07e729e5052716a840da543
Elixer <everett.rumery@gmail.com>
2024-11-22 14:05:06 -0500

    hloidfgh

--- src/Driver.java
+++ src/Driver.java
@@ -1,11 +1,7 @@
 public class Driver {
     public static void main(String[] args) throws Exception {
-        Prison abc = new Prison(100);
-        System.out.println(abc);
-
-
-
-
+        Prison abc = new Prison(10);
+        
 
 
 
--- src/Prison.java
+++ src/Prison.java
@@ -6,10 +6,24 @@ public class Prison {
     public Prison(int boxnum) {
         boxes = new int[boxnum];
         for (int i = 0; i < boxnum; i++) {
-            boxes[i] = (int) (Math.random() * boxnum);
+            boxes[i] = i;
         }
+        this.shuffle();
     }
 
+    public boolean followSmartPath(int start, boolean show) { //VERY WRONG
+        String output = "";
+        for(int i = 0; i < boxes.length; i++) { //the rate will be determined here VERY WRONG
+            output += boxes[i];
+            if(start == boxes[i]) {
+
+                return true;
+            }
+        }
+        return false;
+    }
+
+    
     @Override
     public String toString() {
         String output = "";
@@ -18,13 +32,46 @@ public class Prison {
                 output+="\n";
             }
             if(i < 10) { //box #
-                output+= "[0" + i + ", " + boxes[i] + "]";
+                output+= "[0" + i + ", " + boxes[i] + "] ";
             }
             else{ //box #
-                output+= "[" + i + ", " + boxes[i] + "]";
+                output+= "[" + i + ", " + boxes[i] + "] ";
             }
         }
         return output;
 
+    }
+
+
+    public void shuffle() {
+        int tempnum = 0;
+        int tempindex = 0;
+        int tempnum2 = 0;
+        int tempindex2 = 0;
+
+        for(int i = 0 ; i< boxes.length; i++) { //dont use i here
+
+            tempindex = (int) (Math.random()*boxes.length);
+            tempnum = boxes[tempindex];
+
+            tempindex2 = (int) (Math.random()*boxes.length);
+            tempnum2 = boxes[tempindex2];
+            
+            boxes[tempindex] = tempnum2;
+            boxes[tempindex2] = tempnum;
+        }
+    }
+    public void reset() {
+        for(int i = 0; i < boxes.length; i++) {
+            boxes[i] = i;
+        }
+    }
+
+    public int[] getBoxes() {
+        return boxes;
+    }
+
+    public void setBoxes(int[] boxes) {
+        this.boxes = boxes;
     }
 }
