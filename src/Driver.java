bc1080d8a6267dd9825c414aacf0c09596209e98
ElixerGlint <everett.rumery@gmail.com>
2024-11-27 20:53:42 -0500

    fassdgsdgh

--- src/Driver.java
+++ src/Driver.java
@@ -1,9 +1,8 @@
 public class Driver {
     public static void main(String[] args) throws Exception {
-        Prison abc = new Prison(5);
+        Prison abc = new Prison(1000);
 
-        System.out.println(abc.simulate(1000000, false));
-        System.out.println(abc.simulate(1000, true));
+    
 
 
 
